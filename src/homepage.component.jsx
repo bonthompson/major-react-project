7e6d8070d2be6d8e247a9028264977dd9bdc6ad1
bonthompson <bonniethompson123456@gmail.com>
2021-04-08 09:21:59 +0200

    First commit

---
+++ src/homepage.component.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import './homepage.styles.scss'
+
+const Homepage = () => (
+  <div className='hompeage'>
+    <div className='directory-menu'>
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='title'>Hats</h1>
+          <h2 className='subtitle'>Shop</h2>
+        </div>
+      </div>
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='title'>Hats</h1>
+          <h2 className='subtitle'>Shop</h2>
+        </div>
+      </div>
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='title'>Hats</h1>
+          <h2 className='subtitle'>Shop</h2>
+        </div>
+      </div>
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='title'>Hats</h1>
+          <h2 className='subtitle'>Shop</h2>
+        </div>
+      </div>
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='title'>Hats</h1>
+          <h2 className='subtitle'>Shop</h2>
+        </div>
+      </div>
+    </div>
+  </div>
+)
+
+export default Homepage
