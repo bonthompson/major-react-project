a4e89d3dba0aeea2ad84afe76391882e80753373
bonthompson <bonniethompson123456@gmail.com>
2021-04-08 19:06:03 +0200

    added button and input components and added nodemodules to gitignore

--- src/App.js
+++ src/App.js
@@ -4,13 +4,18 @@ import { Route, Switch } from 'react-router-dom'
 import './App.css'
 import Homepage from './pages/homepage/homepage.component'
 import Shoppage from './pages/shop/shop.component'
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
+
+import Header from './components/header/header.component'
 
 function App () {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={Homepage} />
         <Route exact path='/shop' component={Shoppage} />
+        <Route exact path='/signin' component={SignInAndSignUpPage} />
 
       </Switch>
     </div>
